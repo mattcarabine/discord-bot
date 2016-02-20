8aa6748149d4b876615be9eb8145f2739d5aa0bd
mattcarabine <matt.carabine@couchbase.com>
2016-02-20 19:29:23 +0000

    Fix current-game bug

--- discord-bot/discord-bot.py
+++ discord-bot/discord-bot.py
@@ -9,6 +9,10 @@ client.login(os.environ.get('DISCORD_EMAIL'),
              os.environ.get('DISCORD_PASSWORD'))
 
 l = LeagueBot(client)
+"""
+Better way to do this - have a list of bots all with their own prefix, each then has a subsequent command associated.
+"""
+
 commands = {r'^leaguebot add (\w|\s){3,16}$': l.add_player,
             r'^leaguebot list$': l.print_players,
             r'^leaguebot current-games$': l.get_current_games,
